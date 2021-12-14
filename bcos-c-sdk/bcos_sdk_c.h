6032af011c3947084acc166b3849a3d93da067e5
octopus <912554887@qq.com>
2021-12-14 19:31:04 +0800

    add bcos_sdk_create_by_config_file interface

--- bcos-c-sdk/bcos_sdk_c.h
+++ bcos-c-sdk/bcos_sdk_c.h
@@ -9,12 +9,17 @@ extern "C" {
 
 // create bcos sdk object by config
 void* bcos_sdk_create(struct bcos_sdk_c_config* config);
+// create bcos sdk object by config file
+void* bcos_sdk_create_by_config_file(const char* config_file);
 // start the bcos sdk
 void bcos_sdk_start(void* sdk);
 // stop the bcos sdk object
 void bcos_sdk_stop(void* sdk);
 // destroy the bcos sdk object
 void bcos_sdk_destroy(void* sdk);
+
+// TODO: add impl, register block notifier of the group
+void bcos_sdk_register_block_notifier(const char* group);
 
 #ifdef __cplusplus
 }
