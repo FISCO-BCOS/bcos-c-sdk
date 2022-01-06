b5660c07d5b26a692f3f9d920aac9b3da779fb6a
octopus <912554887@qq.com>
2022-01-06 23:19:27 +0800

    remove bcos-boostssl dir and use bcos-boostssl module instead

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -25,14 +25,20 @@ hunter_config(
     40f7f34551012f68e822664a0b179e7e6cac5a97
 )
 
+hunter_config(bcos-utilities VERSION 3.0.0-rc2-local
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/fbe6d67de2e5d17f2ad35fd1730397239aa9373a.tar.gz
+	SHA1 9cf973988b201d187e89aba69b2dd6d843a9612c
+    CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
+)
+
 hunter_config(bcos-boostssl
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/4f60c9b070242b91731e239f8e30a0fce76da1a8.tar.gz
-	SHA1 e095c2f6544f5b0c06ec6314bc95772a6d3a2fc4
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/f5398f6dc3eb6f9e722a3a7dcbf77d40f7254a09.tar.gz
+	SHA1 5de463a068aa479f0bfd6d8ddc81709c5003d7be
 )
 
 hunter_config(bcos-cpp-sdk
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/d0c49009bb6f0a7cc37e7381174641dc17f0c616.tar.gz
-	SHA1 169863245e68856485eb711b115a8991e20b0786
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/b7b94fcfaebb141c2ecf9694174162327598a483.tar.gz
+	SHA1 0b1eafe95c2312a9416fadd711deac4952deca92
 )
