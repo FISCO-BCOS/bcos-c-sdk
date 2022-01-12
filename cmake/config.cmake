9d856bd98cecb1da9cc79a24e2f0079589b6f375
ywy2090 <912554887@qq.com>
2022-01-12 18:25:48 +0800

    add ci for windows (#32)
    Former-commit-id: 2558cf7407c3247c5f83c6dc072532c1ecda2d26 [formerly 2558cf7407c3247c5f83c6dc072532c1ecda2d26 [formerly 75717bcdd51dd3adf3d26932246f94b198ca4db2]]
    Former-commit-id: 63e65e0a28064f9013a2063c4bf391de06ca4e00
    Former-commit-id: ceafc2bd0d6b373734dfbcad7f484e12f4bf36ff

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -26,19 +26,19 @@ hunter_config(
 )
 
 hunter_config(bcos-utilities VERSION 3.0.0-rc2-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/fbe6d67de2e5d17f2ad35fd1730397239aa9373a.tar.gz
-	SHA1 9cf973988b201d187e89aba69b2dd6d843a9612c
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/b94375d933cc65c348b5c219f2ed821a5f66d9b1.tar.gz
+	SHA1 2ae9c579e924ccb67f07c9626d4d748bc60c9cef
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(bcos-boostssl
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/f5398f6dc3eb6f9e722a3a7dcbf77d40f7254a09.tar.gz
-	SHA1 5de463a068aa479f0bfd6d8ddc81709c5003d7be
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/ef6705e59ea07f1dac8acc62e31cd49731df6a47.tar.gz
+	SHA1 8b7da727c4410beaa5ce9310bdcdd08d91687132
 )
 
 hunter_config(bcos-cpp-sdk
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/b7b94fcfaebb141c2ecf9694174162327598a483.tar.gz
-	SHA1 0b1eafe95c2312a9416fadd711deac4952deca92
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/22d640b5535ff7ac7972db9cb497f38aa0f122a6.tar.gz
+	SHA1 f38fd137d9bde1cd8a7e1e67f2343bf2523d844a
 )
