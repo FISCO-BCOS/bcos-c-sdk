558fd21ee949f0a380f78491e91645b50957e9e1
octopus <912554887@qq.com>
2022-01-21 23:42:21 +0800

    update deps url

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -31,14 +31,20 @@ hunter_config(bcos-utilities VERSION 3.0.0-rc2-local
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
+hunter_config(bcos-crypto VERSION 3.0.0-rc3-local
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-crypto/archive/e2d2af7195a725eeab31da679f12d20c4838e0a1.tar.gz
+	SHA1 e7ed758bfbcbd6db3a9d6dd99ba5aca022eab7c5
+    CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON SM2_OPTIMIZE=ON
+)
+
 hunter_config(bcos-boostssl
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/ef6705e59ea07f1dac8acc62e31cd49731df6a47.tar.gz
-	SHA1 8b7da727c4410beaa5ce9310bdcdd08d91687132
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/685288e31c45ab983fc68b789ba81838948e2991.tar.gz
+	SHA1 ba076ca3770b5a59e58dcc1772c99718d29f3854
 )
 
 hunter_config(bcos-cpp-sdk
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/a06403dbd06458a0974933db7161f3d55b04e1ad.tar.gz
-	SHA1 4c012df89a9bbb1b1c71d43373d7bc56f45adf9d
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/f81727f912280d3d6814fde44c21116fc04982d4.tar.gz
+	SHA1 96c003fec575fe4f6b66997e99b9dd9c34a24c62
 )
