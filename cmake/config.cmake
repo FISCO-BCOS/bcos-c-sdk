c850523df8cbd14e6f6d8d120318877aef66e5bf
octopus <912554887@qq.com>
2022-06-30 16:51:41 +0800

    udpate bcos-boostssl、bcos-cpp-sdk

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,8 +1,8 @@
-#hunter_config(
-#    OpenSSL VERSION "tassl_1.1.1-local"
-#    URL https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/deps/tassl/tassl_1.1.1b_v1.4-fe885b93.tar.gz
-#    SHA1 3f207bbd209bec67a5aca27c0ffafe9c57ad1884
-#)
+# hunter_config(
+# OpenSSL VERSION "tassl_1.1.1-local"
+# URL https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/deps/tassl/tassl_1.1.1b_v1.4-fe885b93.tar.gz
+# SHA1 3f207bbd209bec67a5aca27c0ffafe9c57ad1884
+# )
 
 hunter_config(
     Boost VERSION "1.76.0-local"
@@ -27,25 +27,25 @@ hunter_config(
 
 hunter_config(bcos-utilities VERSION 1.0.0-rc3-local
     URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/632cbeb0412d6ffa3c5217630fe6039f4092f3a4.tar.gz
-	SHA1 37b46cd3174ec1b66e28f19e6e8f31f209b61e4b
+    SHA1 37b46cd3174ec1b66e28f19e6e8f31f209b61e4b
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(bcos-crypto VERSION 1.0.0-rc3-local
     URL https://${URL_BASE}/FISCO-BCOS/bcos-crypto/archive/34ca9b28f3d2f31948c118a70523df6fa4695e23.tar.gz
-	SHA1 64e7b64652ccad212a497d9247fd238fea75578a
+    SHA1 64e7b64652ccad212a497d9247fd238fea75578a
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(bcos-boostssl
     VERSION 1.0.0-rc4-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/1e02d74bd4bdebd6bada02117e92df892e65a53c.tar.gz
-    SHA1 55d6421eb8001dca0067881f3c16d04cef8f6596
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/f628800173796df5eaab3366fd75d929a08eaa5b.tar.gz
+    SHA1 22d28dcacfc01fd11573fc3f68b203707b56692a
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON ARCH_NATIVE=${ARCH_NATIVE} URL_BASE=${URL_BASE}
 )
 
 hunter_config(bcos-cpp-sdk
-	VERSION 3.0.0-rc4-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/da0874f6fc3545694b2c9aa25518479ae1403aae.tar.gz
-    SHA1 b3c98c4ef15dcb900b44adf06029ace9c9a36626
+    VERSION 3.0.0-rc4-local
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/970e985aa2b9e10b71120bfaa4dd3f659d09c205.tar.gz
+    SHA1 1f9c5c85dcd27c60879ce689ba449eb9d8cb7965
 )
