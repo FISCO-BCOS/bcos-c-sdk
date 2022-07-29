47535f17fac96b38e9d0de485006783cb9738bac
ylxiao5 <ylxiao5@iflytek.com>
2022-07-29 13:50:19 +0800

    peafect adapter code

--- bindings/go/csdk/csdk_wrapper.go
+++ bindings/go/csdk/csdk_wrapper.go
@@ -5,10 +5,10 @@ package csdk
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/linux -lbcos-c-sdk
 // #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/libs/win -lbcos-c-sdk
 // #cgo CFLAGS: -I./
-// #include "include/bcos_sdk_c.h"
-// #include "include/bcos_sdk_c_error.h"
-// #include "include/bcos_sdk_c_rpc.h"
-// #include "include/bcos_sdk_c_uti_tx.h"
+// #include "../../../bcos-c-sdk/bcos_sdk_c.h"
+// #include "../../../bcos-c-sdk/bcos_sdk_c_error.h"
+// #include "../../../bcos-c-sdk/bcos_sdk_c_rpc.h"
+// #include "../../../bcos-c-sdk/bcos_sdk_c_uti_tx.h"
 // #include <stdlib.h>
 // void on_recv_resp_callback(struct bcos_sdk_c_struct_response *);
 import "C"
@@ -16,19 +16,22 @@ import "C"
 import (
 	"encoding/hex"
 	"fmt"
-	"time"
-	"unsafe"
-
 	"github.com/sirupsen/logrus"
+	"unsafe"
 )
 
-type SDK struct {
+type CSDK struct {
 	c          unsafe.Pointer
 	SMCrypto   bool
 	WASM       bool
 	ChainID    *C.char
 	GroupID    *C.char
-	PrivateKey unsafe.Pointer
+	NodeID    *C.char
+	Callback   *C.bcos_sdk_c_struct_response_cb
+}
+
+type ChanData struct {
+	Data    chan string
 }
 
 //export on_recv_resp_callback
@@ -36,11 +39,14 @@ func on_recv_resp_callback(resp *C.struct_bcos_sdk_c_struct_response) {
 	if int(resp.error) != 0 {
 		logrus.Errorf("\t something is wrong, error: %d, errorMessage: %s\n", resp.error, resp.desc)
 	} else {
-		logrus.Infof(" \t recv rpc resp from server ==>>>> resp: %s\n", C.GoString((*C.char)(resp.data)))
+		data := C.GoString((*C.char)(resp.data))
+		chanData := (*ChanData)(unsafe.Pointer(resp.context))
+		logrus.Infof(" \t  *****recv rpc resp from server ==>>>> resp -> data: %s\n", data)
+		chanData.Data <- data
 	}
 }
 
-func NewSDK(config *C.struct_bcos_sdk_c_config, groupID string, privateKey []byte) *SDK {
+func NewSDK(config *C.struct_bcos_sdk_c_config, groupID string) *CSDK {
 	defer C.free(unsafe.Pointer(config))
 	sdk := C.bcos_sdk_create(config)
 	if sdk == nil {
@@ -54,7 +60,7 @@ func NewSDK(config *C.struct_bcos_sdk_c_config, groupID string, privateKey []byt
 	group := C.CString(groupID)
 	C.bcos_sdk_get_group_wasm_and_crypto(sdk, group, &wasm, &smCrypto)
 	chainID := C.bcos_sdk_get_group_chain_id(sdk, group)
-	return &SDK{
+	return &CSDK{
 		c:        sdk,
 		SMCrypto: smCrypto != 0,
 		WASM:     wasm != 0,
@@ -63,7 +69,7 @@ func NewSDK(config *C.struct_bcos_sdk_c_config, groupID string, privateKey []byt
 	}
 }
 
-func NewSDKByConfigFile(configFile string, groupID string, privateKey []byte) *SDK {
+func NewSDKByConfigFile(configFile string, groupID string) *CSDK {
 	config := C.CString(configFile)
 	sdk := C.bcos_sdk_create_by_config_file(config)
 	if sdk == nil {
@@ -77,7 +83,7 @@ func NewSDKByConfigFile(configFile string, groupID string, privateKey []byte) *S
 	group := C.CString(groupID)
 	C.bcos_sdk_get_group_wasm_and_crypto(sdk, group, &wasm, &smCrypto)
 	chainID := C.bcos_sdk_get_group_chain_id(sdk, group)
-	return &SDK{
+	return &CSDK{
 		c:        sdk,
 		SMCrypto: smCrypto != 0,
 		WASM:     wasm != 0,
@@ -86,37 +92,41 @@ func NewSDKByConfigFile(configFile string, groupID string, privateKey []byte) *S
 	}
 }
 
-func (sdk *SDK) GetBlockLimit() int {
+func (sdk *CSDK) Close() {
+	C.bcos_sdk_stop(sdk.c)
+	C.bcos_sdk_destroy(sdk.c)
+	C.free(unsafe.Pointer(sdk.GroupID))
+	C.free(unsafe.Pointer(sdk.ChainID))
+}
+
+func (sdk *CSDK) GetBlockLimit() int {
 	return int(C.bcos_rpc_get_block_limit(sdk.c, sdk.GroupID))
 }
 
-func (sdk *SDK) bcos_rpc_get_group_list() {
-	C.bcos_rpc_get_group_list(sdk.c, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), sdk.c)
+func (sdk *CSDK) GetGroupList(chanData *ChanData) {
+	C.bcos_rpc_get_group_list(sdk.c, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), unsafe.Pointer(chanData))
 }
 
-func (sdk *SDK) bcos_rpc_get_sealer_list() {
-	C.bcos_rpc_get_sealer_list(sdk.c, sdk.GroupID, nil, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), nil)
+func (sdk *CSDK) GetSealerList(chanData *ChanData) {
+	C.bcos_rpc_get_sealer_list(sdk.c, sdk.GroupID, nil, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), unsafe.Pointer(chanData))
 }
 
-func (sdk *SDK) bcos_rpc_get_block_number() {
-	C.bcos_rpc_get_block_number(sdk.c, sdk.GroupID, nil, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), sdk.c)
+func (sdk *CSDK) GetBlockNumber(chanData *ChanData) {
+	C.bcos_rpc_get_block_number(sdk.c, sdk.GroupID, nil, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), unsafe.Pointer(chanData))
 }
+
+//func (sdk *CSDK) GetBlockHashByNumber(hc *ChanData, blockNumber int) {
+//	C.bcos_rpc_get_block_hash_by_number(sdk.c, sdk.GroupID, nil, C.bcos_sdk_c_struct_response_cb(C.on_recv_resp_callback), unsafe.Pointer(hc))
+//}
 
 func blockNotify(group *C.char, block_number C.int64_t, ctx unsafe.Pointer) {
 	fmt.Printf("Hello\n")
 }
 
-func (sdk *SDK) RegisterBlockNotify(groupID string, context unsafe.Pointer) {
+func (sdk *CSDK) RegisterBlockNotify(groupID string, context unsafe.Pointer) {
 	cGroupID := C.CString(groupID)
 	defer C.free(unsafe.Pointer(cGroupID))
 	//C.bcos_sdk_register_block_notifier(sdk.c, cGroupID, unsafe.Pointer(context), blockNotify)
-}
-
-func (sdk *SDK) Close() {
-	C.bcos_sdk_stop(sdk.c)
-	C.bcos_sdk_destroy(sdk.c)
-	C.free(unsafe.Pointer(sdk.GroupID))
-	C.free(unsafe.Pointer(sdk.ChainID))
 }
 
 // tx functions
@@ -134,23 +144,4 @@ func CreateSignedTransaction(groupID, chainID, to string, data []byte, abi strin
 	defer C.free(unsafe.Pointer(cData))
 	defer C.free(unsafe.Pointer(cAbi))
 	return C.bcos_sdk_create_transaction_data(cGroupID, cChainID, cTo, cData, cAbi, cBlockLimit)
-}
-
-func main() {
-	sdk := NewSDKByConfigFile("./config_sample.ini", "group0", nil)
-	logrus.Infof("group id:%s", C.GoString(sdk.GroupID))
-
-	sdk.bcos_rpc_get_group_list()
-	time.Sleep(3 * time.Second)
-
-	sdk.bcos_rpc_get_sealer_list()
-	time.Sleep(3 * time.Second)
-
-	sdk.bcos_rpc_get_block_number()
-	time.Sleep(3 * time.Second)
-
-	logrus.Infof("Get Block Limit:%d", sdk.GetBlockLimit)
-	logrus.Infof("chain id:%s", C.GoString(sdk.ChainID))
-
-	defer sdk.Close()
 }
