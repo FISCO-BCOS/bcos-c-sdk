9a5222ea1f2e3c96f4d1001a24474a6b7b5b324c
lucasli <410567249@qq.com>
2023-01-05 10:30:07 +0800

    add function named bcos_sdk_use_hsm_keypair_by_keyindex_and_password (#119)

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -51,8 +51,8 @@ hunter_config(bcos-boostssl
 
 hunter_config(bcos-cpp-sdk
     VERSION 3.0.1-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/82aa79f74e180b4644d04b80e21058832e3163bd.tar.gz
-    SHA1 13d23e84a4ca3ff24a1f004b935ab6fedef85c58
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/1f853d7244a0d7cc771f08b3cea92d9e168493ae.tar.gz
+    SHA1 d75f6381d456b4070c3a530d852e6981fa6e5206
 )
 
 hunter_config(range-v3 VERSION 1.0.0
