181381ea67d211b925363b0d4f8527808bebd3c0
lucasli <410567249@qq.com>
2023-02-10 15:54:31 +0800

    add code of HSM (#134)

---
+++ cmake/InstallSDF.cmake
@@ -0,0 +1,28 @@
+include(ExternalProject)
+include(GNUInstallDirs)
+
+set(SDF_LIB_NAME "libsdf-crypto.a")
+
+ExternalProject_Add(libsdf
+    PREFIX ${CMAKE_SOURCE_DIR}/deps
+    GIT_REPOSITORY https://${URL_BASE}/WeBankBlockchain/hsm-crypto.git
+    GIT_TAG        0693c45167a932266f8a1c11f3ef83d982e8334d
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    BUILD_IN_SOURCE true
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+)
+
+ExternalProject_Get_Property(libsdf INSTALL_DIR)
+
+set(HSM_INCLUDE_DIR ${INSTALL_DIR}/include/)
+file(MAKE_DIRECTORY ${HSM_INCLUDE_DIR})  # Must exist.
+
+set(SDF_LIB "${INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/${SDF_LIB_NAME}")
+
+add_library(SDF STATIC IMPORTED GLOBAL)
+set_property(TARGET SDF PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${HSM_INCLUDE_DIR})
+set_property(TARGET SDF PROPERTY IMPORTED_LOCATION ${SDF_LIB})
+add_dependencies(SDF libsdf)
+unset(INSTALL_DIR)
