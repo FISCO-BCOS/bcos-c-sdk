aa5c557276a5140a6297e6a6d02091615fc1d294
octopus <912554887@qq.com>
2023-03-29 17:15:32 +0800

    udpate cpp-sdk to fix extraData not set bug

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -51,8 +51,8 @@ hunter_config(bcos-boostssl
 
 hunter_config(bcos-cpp-sdk
     VERSION 3.3.0-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/58f0efc1ad08e740955d0a389833b962b63f5644.tar.gz
-    SHA1 0d63f5d4cf6384d9c34380c7c3d337ca4df8a217
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/d47a751fe5f840fd22b38bcf9bedd61e1b1ad478.tar.gz
+    SHA1 debd13410b98474dc0e4d55b7b08d233999fa0e5
 )
 
 hunter_config(range-v3 VERSION 1.0.0
